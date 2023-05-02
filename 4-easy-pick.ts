7a4cb4fe2e1686f2c308b0a7be999768fb5c7dfa
cuzknothz <91480011+cuzknothz@users.noreply.github.com>
2023-05-02 08:52:49 +0700

    🥕

---
+++ 4-easy-pick.ts
@@ -0,0 +1,14 @@
+// Implement the built-in `Pick<T, K>` generic without using it.
+
+//Code
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+//
+
+interface Todo {
+  title: string;
+  description: string;
+}
+
+type PickTitle = MyPick<Todo, "title">;
